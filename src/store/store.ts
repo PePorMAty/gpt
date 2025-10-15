ba8aea4aac86e884cab55b9d1bda0710a8ef9046
pepormaty <pepormaty@gmail.com>
2025-10-15 18:42:11 +0700

    middleware types

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { useDispatch, useSelector } from "react-redux";
+import type { Middleware } from 'redux';
 import gptReducer from "./slices/gpt/gpt-slice";
 
 export const store = configureStore({
@@ -12,18 +13,17 @@ export const store = configureStore({
         ignoredActions: ["persist/PERSIST"],
       },
     }).concat(
-      // Логирование действий
-      (store) => (next) => (action) => {
+      // Логирование действий с явными типами
+      ((store) => (next) => (action) => {
         console.log("Dispatching:", action);
         const result = next(action);
         console.log("Next state:", store.getState());
         return result;
-      }
+      }) as Middleware
     ),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
 export const useAppSelector = useSelector.withTypes<RootState>();
